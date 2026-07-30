7e3cb3fd1d4aec8212f9e5ce24a1c1841de1a696
SheenaDeguzman <deguzmansheena30@gmail.com>
2026-07-31 00:10:48 +0800

    added PostCSS Color Plugin

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+import postcssColorFunctionalNotation from 'postcss-color-functional-notation';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,6 +11,13 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    css: {
+      postcss: {
+        plugins: [
+          postcssColorFunctionalNotation({ preserve: false })
+        ]
+      }
+    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
